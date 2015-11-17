c388e6db911003b299e4bc2dae2d7365bebcbda8
Markus Meier <m2meier@inno.ch>
2015-11-17 12:36:32 +0200

    Prepare for github

---
+++ i3/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  i3
+//
+//  Copyright (c) 2015 Innotronic Ingenieurbüro GmbH. All rights reserved.
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+    
+    
+    func application( application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        return true
+    }
+    
+    func applicationWillResignActive( application: UIApplication )
+    {
+    }
+
+    func applicationDidEnterBackground( application: UIApplication )
+    {
+    }
+
+    func applicationWillEnterForeground( application: UIApplication )
+    {
+    }
+
+    func applicationDidBecomeActive( application: UIApplication )
+    {
+    }
+
+    func applicationWillTerminate( application: UIApplication )
+    {
+    }
+}
+
